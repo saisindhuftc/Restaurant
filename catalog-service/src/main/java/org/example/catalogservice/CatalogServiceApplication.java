12d9a04331938ae92420eaaddb282cd0d6f95ea0
saisindhuftc <sai.subbisetty_ftc@external.swiggy.in>
2024-10-21 09:39:09 +0530

    first commit

---
+++ catalog-service/src/main/java/org/example/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,9 @@
+package org.example.catalogservice;
+
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+}
